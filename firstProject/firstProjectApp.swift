d62f0bd14e3eccdd460eebdf42710a715a900af7
Ohoud <ohoodbawaked@gmail.com>
2024-07-24 10:04:35 +0300

    Initial Commit

---
+++ firstProject/firstProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  firstProjectApp.swift
+//  firstProject
+//
+//  Created by Ohoud  on 18/01/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct firstProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
